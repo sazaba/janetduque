87f4dee3c59796d93a3c661a33e837e81fb6fea1
sazaba <santiagozappab96@gmail.com>
2026-03-15 19:47:35 -0500

    tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,25 @@ module.exports = {
         serif: ['var(--font-serif)'],
         sans: ['var(--font-sans)'],
       },
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(32px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        fadeInRight: {
+          '0%': { opacity: '0', transform: 'translateX(32px)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        textReveal: {
+          '0%': { opacity: '0', clipPath: 'inset(0 100% 0 0)' },
+          '100%': { opacity: '1', clipPath: 'inset(0 0 0 0)' },
+        },
+        draw: {
+          '0%': { strokeDasharray: '100', strokeDashoffset: '100', opacity: '0' },
+          '10%': { opacity: '1' },
+          '100%': { strokeDasharray: '100', strokeDashoffset: '0', opacity: '1' },
+        }
+      }
     },
   },
   plugins: [],
